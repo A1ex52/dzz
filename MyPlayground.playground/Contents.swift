70013adf2b25426ae0cfa31512aeb18949339666
Alex <alexk77752@mail.ru>
2025-12-03 22:01:28 +0300

    Первое дз на гит

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,15 @@
+let count: UInt = 15
+let apples: String
 
+if count == 1  {
+    apples = "яблоко"
+} else if count == 15 {
+    apples = "яблок"
+} else if count == 154 {
+    apples = "яблока"
+} else {
+    apples = "яблок"
+}
 
-
+let result = "В корзине \(count) \(apples)"
+print(result)
