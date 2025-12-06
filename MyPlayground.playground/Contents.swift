1b23b7bb2be09a7306a04e2a6c0c09c40904ea9b
Alex <alexk77752@mail.ru>
2025-12-06 22:39:09 +0300

    HW3

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,18 @@
-let array = [1, 8, 4, 10, -254, 800, 1005, 12, -2, 32]
-let mmm = array.max()! - array.min()!
-print(mmm)
+let count: UInt = 156
+let apples: String
 
+let lastTwo = count % 100
+let lastOne = count % 10
 
+if lastTwo >= 11 && lastTwo <= 14 {
+   apples = "яблок"
+} else if lastOne == 1 {
+   apples = "яблоко"
+} else if lastOne >= 2 && lastOne <= 4 {
+   apples = "яблока"
+} else {
+   apples = "яблок"
+}
+
+let result = "В корзине \(count) \(apples)"
+print(result)
